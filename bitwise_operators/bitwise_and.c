f881e9396106070ca8df4d797d41544fd6e85474
Kartik Deshmukh <kartik@mellob.co>
2022-11-18 11:38:26 +0530

    Added Bitwise Operators

---
+++ bitwise_operators/bitwise_and.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("Input values of a and b: \n");
+    scanf("%d %d", &a, &b);
+    printf("Result of a & b is %d\n", (a&b));
+    return 0;
+}
