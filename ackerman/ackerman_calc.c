80f8e9813233f2735f174f1bcdd1a655dd680039
Kartik Deshmukh <kartik@mellob.co>
2023-03-02 22:03:24 +0530

    Added patterns

--- ackerman/ackerman_calc.c
+++ ackerman/ackerman_calc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int ackerman_calc();
+long count;
 
 int main(){
     int m, n;
@@ -8,7 +9,7 @@ int main(){
     scanf("%d%d", &m, &n);
     int result = ackerman_calc(m, n);
     printf("\nResult: %d\n", result);
-
+    printf("Number of recursions:- %ld\n", count);
 }
 
 int ackerman_calc(int m, int n){
@@ -23,4 +24,5 @@ int ackerman_calc(int m, int n){
     else if(m > 0 && n > 0){
         ackerman_calc(m-1, ackerman_calc(m, n-1));
     }
+    //count = count + 1;
 }
